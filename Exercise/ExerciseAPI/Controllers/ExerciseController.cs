395f78960f50a714a3d9fae78eb8ea65167208de
Mehrdad <m.dolatabadi@emofid.com>
2021-10-23 16:56:35 +0330

    init

---
+++ Exercise/ExerciseAPI/Controllers/ExerciseController.cs
@@ -0,0 +1,217 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExerciseAPI.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class ExerciseController : ControllerBase
+    {
+        //max timeout allowed for retriving, merging, duplicate removing and sorting external URLs information
+        //due to delay to response and write output to stream for large lists, this number is below the 500 (target)
+        private static readonly int timeout = 460;
+
+        private readonly ILogger<ExerciseController> _logger;
+
+        public ExerciseController(ILogger<ExerciseController> logger)
+        {
+            _logger = logger;
+        }
+        /// <summary>
+        /// Returns a json response including the merged and sorted numbers extracted from URLs
+        /// </summary>
+        /// <param name="u">List of URLs to fetch source numbers from</param>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Route("numbers")]
+        [HttpGet]
+
+        public ActionResult numbers([FromQuery] string[] u)
+        {
+            if (u.Length == 0)
+            {
+                return Problem(detail: $"The input was empty. you need to privide the URLs.", statusCode: 400);
+            }
+            //the locker to lock on across tasks
+            object _locker = new object();
+            try
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Restart();
+                var result = new List<int>();
+                var Third_list = new List<int>();
+                object set_lock = new object();
+                var cancel = new CancellationTokenSource();
+                var token = cancel.Token;
+
+
+                var tasks = new List<Task>() { };
+                //creating a task foreach URL
+                foreach (var uri in u)
+                {
+
+                    var t1 = Task.Factory.StartNew(() =>
+                    {
+                        if (!IsUrlValid(uri))
+                        {
+                            Console.WriteLine($"URL isn't valid {uri}");
+                            return;
+                        }
+                        var numbers = new List<int>();
+                        try
+                        {
+                            //sort and remove dups from local result
+                            numbers = get_uri_numbers(uri).ToHashSet().OrderBy(x => x).ToList();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("get request failed");
+                            //logging ex
+                            return;
+                        }
+
+                        var local_index = 0;
+                        var result_index = 0;
+                        lock (_locker)
+                        {
+                            var local_count = numbers.Count;
+                            Third_list = new List<int>();
+                            var result_count = result.Count;
+                            while (true)
+                            {
+                                //handling the cancelation operation within the loop
+                                if (cancel.IsCancellationRequested)
+                                {
+
+                                    return;
+                                }
+                                if (local_index < local_count && (result_index >= result_count || numbers[local_index] < result.ElementAt(result_index)))
+                                {
+                                    //preventing to push duplicates
+                                    if (!Third_list.Any() || Third_list.Last() != numbers[local_index])
+                                        Third_list.Add(numbers[local_index]);
+                                    local_index++;
+                                }
+
+                                else if (result_index < result_count && (local_index >= local_count || numbers[local_index] > result.ElementAt(result_index)))
+                                {
+                                    //preventing to push duplicates
+                                    if (!Third_list.Any() || Third_list.Last() != result.ElementAt(result_index))
+                                        Third_list.Add(result.ElementAt(result_index));
+                                    result_index++;
+                                }
+                                //move both pointers and insert duplicate value only once
+                                else if (result_index < result_count && local_index < local_count && numbers[local_index] == result.ElementAt(result_index))
+                                {
+                                    Third_list.Add(result.ElementAt(result_index));
+                                    local_index++;
+                                    result_index++;
+                                }
+                                //break if traversing both lists is finished
+                                if (result_index >= result_count && local_index >= local_count)
+                                {
+                                    break;
+                                }
+
+                            }
+                            // now the Third_list is ordered but we need to update result aswell for the use of the other tasks
+                            result = new List<int>(Third_list);
+
+                        }
+
+
+                    }, cancellationToken: token);
+                    tasks.Add(t1);
+
+
+                }
+                //the time which was needed for creation of tasks
+                var spent_time = sw.Elapsed.TotalMilliseconds;
+                //calculatig the remaining time to fetch, merge and sort result
+                var remaining_time = timeout - (int)spent_time;
+                if (remaining_time > 0)
+                {
+                    Task.WaitAll(tasks.ToArray(), remaining_time, cancel.Token);
+                }
+                //cancel all running tasks
+                cancel.Cancel();
+                Console.WriteLine($"({nameof(numbers)}) {(int)sw.Elapsed.TotalMilliseconds} completed:{tasks.Where(x => x.IsCompletedSuccessfully).Count()}  total_result:{Math.Max(Third_list.Count(), result.Count())}");
+                //return the longer ordered result
+                //to prevent the short result due to timeout in the middle of copying elements to result or just before it
+                return Ok(new { numbers = Third_list.Count() > result.Count() ? Third_list : result });
+
+
+            }
+            catch (Exception ex)
+            {
+
+                return Problem(detail: $"something went wrong. ({ex.ToString()})", statusCode: 500);
+            }
+
+        }
+        /// <summary>
+        /// Checks wether a string is in corect URL format or not
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private bool IsUrlValid(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                return false;
+            string pattern = @"^http(s)?://([\w-]+.)+[\w-]+(/[\w- ./?%&=])?$";
+            Regex reg = new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            return reg.IsMatch(url);
+        }
+        /// <summary>
+        /// Gets numbers from an provided URL address
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        private IEnumerable<int> get_uri_numbers(string uri)
+        {
+
+            using (var client = new HttpClient())
+            {
+
+                //HTTP GET
+                var responseTask = client.GetAsync(uri);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    var stringResult = result.Content.ReadAsStringAsync();
+                    stringResult.Wait();
+                    var dict = JsonSerializer.Deserialize<Dictionary<string, int[]>>(stringResult.Result);
+                    var numbers = dict["numbers"];
+
+
+
+                    return numbers;
+                }
+                else
+                {
+                    Console.WriteLine($"HTTP GET failed.");
+                    //log error
+                }
+            }
+            return new List<int>();
+        }
+
+
+    }
+}
---
+++ Exercise/README.md
@@ -0,0 +1,125 @@
+Travel Audience Challenge - Solution
+============================
+
+Summary
+----
+
+The API exposes endpoint "/numbers". This endpoint receives a list of URLs 
+though GET query parameters. The parameter in use is called "u". It can appear 
+more than once.
+
+	http://yourserver:8080/numbers?u=http://example.com/primes&u=http://foobar.com/fibo
+The Algorithm
+-----
+All the process is done within tasks/threads and main thread just returns the answer.
+
+When the /numbers is called, the service will retrieve each of these URLs if 
+they turn out to be syntactically valid URLs with the use of `Regex` matchings.
+
+
+Since The endpoint needs to return the result always 
+within 500 milliseconds, the solution uses multi-task processing with the max limit of 500 milliseconds for the completion. If a URL takes too long to respond, it must be ignored.
+ After retrieving the numbers through http `get`, each task would `sort` its numbers.
+
+**Notice**: In this step task does not remove duplicate values and only the sorting process would happen.
+I have defined an object as a locker to access and modify cross-thread lists that would be returned as final answers. 
+After locking, the task tries to merge its numbers (and remove duplicates) with the `final list` in `O(n)` with the help of a merging sorted lists algorithm and the result would be stored in a `third list`.
+
+The whole process is a multi-task process like below chart:
+```mermaid
+flowchart TD
+
+    T1_get_url1_numbers --> T1_sort
+    T1_sort --> T1_lock_and_merge
+    T1_lock_and_merge --> final_list=merged_list1
+    final_list=merged_list1 --> return_result
+    T2_get_url2_numbers --> T2_sort
+    T2_sort --> T2_lock_and_merge
+    T2_lock_and_merge --> final_list=merged_list2
+    final_list=merged_list2 --> return_result
+    Tn_get_urln_numbers --> Tn_sort
+    Tn_sort --> Tn_lock_and_merge
+    Tn_lock_and_merge --> final_list=merged_listn
+    final_list=merged_listn --> return_result
+    return_result --> finish
+
+```
+The `3rd list` needs to be copied to `result list` to have updated results.
+
+**Notice**: if the task time outs just before copying the `3rd list` to `final list` or in middle of the copying, `final list` is not complete and the `3rd list` would come with longer and better results, so at the end of the process a comparison between lengthes of `3rd` and `final list` would reveal the best result.
+
+
+
+Since the provided solution, *all the time* can have a relativily and partially correct answer, It only returns an empty list as result only if all URLs returned errors or took too long to respond.
+The `Timeout` is set on parallel tasks running time, therefore The timeout is set regardless of 
+the size of the data.
+
+
+
+
+
+Time Complexity
+---------------------
+since every request localy would be sorted using builtin quick sort sorting algorithm, the order is `O(n*lg(n))`, where `n` is the length of local task's number list. 
+
+
+After local sorting, since the merging of sorted lists happens in a locked process. it's time complexity can be evaluated like a sequential process, therefore the order of this part is:
+    
+    `O(l1+l2+(l1+l2+l3)+....)=O(n*l1+(n-1)*l2+....)`
+
+If the number of `l`s(URLs) are constant number with boundaries the order can be rewitten:
+
+    `o(n^2)`
+
+Overal time complixity:
+
+    `O(n^2+n*lg(n))=O(n^2)`
+
+
+Additional Details
+---------------------
+
+I only used what's provided in the .NET 
+standard library. 
+
+To test with the help of the provided go server (assuming it listens to port 8090), the URLs should be in correct format having the `http` portion:
+
+    http://localhost:8090/primes
+
+So the whole get URL should be like this:
+
+    http://localhost:8080/numbers?u=http://localhost:8090/primes&u=http://localhost:8090/odd&u=http://localhost:8090/rand&u=http://localhost:8090/fibo
+
+Sample oresponse body:
+
+    {"numbers":[1,2,3,5,7,8,9,10,11,13,15,17,19,21,23,24,27,34,76]}
+
+Installation
+--------
+The solution is a .net 5 API project. It can be run in different ways:
+
+1. using dotnet cli: 
+ 
+        ..\Exercise\ExerciseAPI>dotnet run .
+2. using the docker:
+  
+        ..\Exercise>docker build . -t name
+        docker run -detach -p 8080:80 name
+3. using visual studio itself and running the solution.
+
+In all cases the swagger is accessible here:
+
+        http://localhost:8080/swagger/index.html
+    
+And the API end point is:
+
+        http://localhost:8080/numbers
+
+  
+Conclusion
+--------
+
+ Although there can be other and slightly faster solutions with the sorting in the main thread, this solution tries to respect the timeout restriction therefore the whole process is happening within the threads/tasks by facilitating locks and a fast merging algorithm.
+
+
+
